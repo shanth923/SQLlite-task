26dcaf6eebe8b9d84f9289127367e8da9def39c9
shanth kumar <shanth923@gmail.com>
2019-12-17 10:08:14 +0530

    created one object

---
+++ SQLlite/Task1ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  Task1ViewController.swift
+//  SQLlite
+//
+//  Created by R Shantha Kumar on 12/17/19.
+//  Copyright © 2019 R Shantha Kumar. All rights reserved.
+//
+
+import UIKit
+
+class Task1ViewController: UIViewController {
+
+    let task1 = Task1ViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+let name = "ddfdf"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
